9038be9abd5579895305bf53441f4dcbabf3c670
parkg0 <wldud9883@naver.com>
2022-01-19 12:36:44 +0900

    add_2

---
+++ src/com/google/s1/start/StartMain.java
@@ -0,0 +1,12 @@
+package com.google.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB연동 테스트 시작");
+
+		System.out.println("DB연동 테스트 끝");
+
+	}
+
+}
